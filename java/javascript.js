43786fbb2bad23eaec7aa80089b123d41e189354
Malik <laraway@logo-consult.com>
2025-08-11 11:15:00 +0200

    added java

---
+++ java/javascript.js
@@ -0,0 +1,29 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 10000;
+console.log(age);
+
+console.log(3 + 2 - 76 * (1 + 1));
+console.log(23 + 97);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = 20;
+console.log(a);
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
+console.log(actual);
